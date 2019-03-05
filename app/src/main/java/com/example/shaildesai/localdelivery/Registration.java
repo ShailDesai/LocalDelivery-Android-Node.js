8070a545a63ead2748f21a4f5ccdc4bd83587a62
shail desai <shaildesai4594@gmail.com>
2019-03-05 14:19:14 -0400

    added registration library to volley

--- app/src/main/java/com/example/shaildesai/localdelivery/Registration.java
+++ app/src/main/java/com/example/shaildesai/localdelivery/Registration.java
@@ -2,10 +2,14 @@ package com.example.shaildesai.localdelivery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Registration extends AppCompatActivity implements View.OnClickListener {
     private TextView email,user_name,first_name,last_name,phone_number,birth_year,password,confirm_password;
@@ -53,5 +57,44 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
     private void launcRegistration(){
 
     }
+
+
+    private void executeGetpub() throws JSONException {
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        String executeGetpub_url = "https://ptsv2.com/t/dh09i-1551809020/post";
+
+
+
+
+        // Initialize a new JsonObjectRequest instance
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, executeGetpub_url,doInBackground() , new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    response.getString("status");
+                    Log.i("Here is the response",String.valueOf(response.getString("status")));
+                    finish();
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Do something when error occurred
+
+                Log.i("error in getpub ", String.valueOf(error));
+
+            }
+            // Add JsonObjectRequest to the RequestQueue
+
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
 
 }
